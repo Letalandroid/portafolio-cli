7c96bb82f42991a703c65fef9d8449a4d3bc20e1
Letalandroid <carlossoncra@gmail.com>
2022-11-22 11:41:52 -0500

    Add /tutoria

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./Home";
 import AddProyect from "./pages/Add-proyect/Add-proyect";
 import P404 from "./pages/404/404";
+import { Tutoria } from "./pages/tutoria";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add-proyect" element={<AddProyect />} />
+          <Route path="/tutoria" element={<Tutoria />} />
           <Route path="*" element={<P404 />} errorElement={404} />
         </Routes>
       </BrowserRouter>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,8 +2,10 @@ import Portada from "./components/portada/portada";
 import Descripcion from "./components/descripcion/descripcion";
 import Tecnologias from "./components/tecnologias/tecnologias";
 import Proyectos from "./components/proyectos/Proyectos";
+import setTitle from "../../setTitle"
 
 export default function Main() {
+  setTitle("Home")
   return (
     <main>
       <Portada />
